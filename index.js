33fd331423115fe42aea689c8742a698fd28e688
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2023-10-02 19:31:01 +0530

    fixed some of the nav bar elements (fixed the logo to navigate to home page) new feature of loading home page of 1second designed about page

--- index.js
+++ index.js
@@ -17,3 +17,66 @@ window.onclick = function (event) {
         }
     }
 }
+
+
+//image loading blur 
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var blurBackground = document.getElementById('blurBackground');
+    var wrapper = document.getElementById('wrapper');
+  
+    var img = new Image();
+    img.onload = function() {
+      blurBackground.style.backgroundImage = 'url(./img/academic.jpg)';
+      wrapper.style.boxShadow = 'inset 0 0 0 1000px rgba(0, 0, 0, 0)';
+      blurBackground.style.transition = 'filter 1s'; // Added transition property
+      setTimeout(function() {
+        blurBackground.style.filter = 'blur(0)';
+      }, 100);
+    }
+    img.src = './img/academic.jpg';
+  });
+
+
+//scroll in about page
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+  
+  
+
+  
+  
+  
+  
+  
+  
+
+
+
+  
