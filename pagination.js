8ceb41ee2ea07dee596ee3575a06eb1cab0e921e
Vansh Attri <vanshattri123@gmail.com>
2023-10-31 21:13:56 +0530

    added pagination

---
+++ pagination.js
@@ -0,0 +1,65 @@
+$(function () {
+    var numberOfItems = $(".card-content .card").length;
+    var limitPerPage = 3; // How many card items visible in one page
+    var totalPages = Math.ceil(numberOfItems / limitPerPage);
+    var paginationSize = 7; // How many page elements are visible in the pagination
+    var currentPage = 1;
+
+    function showPage(whichPage) {
+        if (whichPage < 1 || whichPage > totalPages) return false;
+
+        currentPage = whichPage;
+
+        $(".card-content .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+        $(".pagination-list li").slice(1, -1).remove();
+
+        getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+            $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
+                .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
+                .attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
+        });
+
+        $(".previous-page").toggleClass("disable", currentPage === 1);
+        $(".next-page").toggleClass("disable", currentPage === totalPages);
+        return true;
+    }
+    function getPageList(totalPages, page, maxLength) {
+        function range(start, end) {
+            return Array.from(Array(end - start + 1), (_, i) => i + start);
+        }
+
+        var sideWidth = maxLength < 9 ? 1 : 2;
+        var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+        var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+
+        if (totalPages <= maxLength) {
+            return range(1, totalPages);
+        }
+
+        if (page <= maxLength - sideWidth - 1 - rightWidth) {
+            return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
+        }
+
+        if (page >= totalPages - sideWidth - 1 - rightWidth) {
+            return range(1, sideWidth).concat(0, range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages));
+        }
+
+        return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
+    };
+
+    showPage(currentPage);
+
+    $(document).on("click", ".pagination-list li.current-page:not(.active)", function () {
+        return showPage(+$(this).text());
+    });
+
+    $(".next-page").on("click", function () {
+        return showPage(currentPage + 1);
+    });
+
+    $(".previous-page").on("click", function () {
+        return showPage(currentPage - 1);
+    });
+
+});
